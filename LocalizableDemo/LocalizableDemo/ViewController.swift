1fc15de5536b86b75ad096e2b115f3132513ab81
Menahem Barouk <menahemb@gini-apps.com>
2018-09-13 12:39:23 +0300

    first commit

---
+++ LocalizableDemo/LocalizableDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LocalizableDemo
+//
+//  Created by Menahem Barouk on 13/09/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
